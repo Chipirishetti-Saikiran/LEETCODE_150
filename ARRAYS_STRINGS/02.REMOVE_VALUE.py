739496a150961289b73f9bf0dfb99deb817cb2ce
Chipirishetti-Saikiran <ch381911@gmail.com>
2025-05-27 11:52:07 +0530

    Added 02.REMOVE_VALUE.py to ARRAYS_STRINGS

---
+++ ARRAYS_STRINGS/02.REMOVE_VALUE.py
@@ -0,0 +1,8 @@
+#OPTIMAL 
+def removeElement(self, nums, val):
+        a=0
+        for i in range(len(nums)):
+            if nums[i]!=val:
+                nums[a]=nums[i]
+                a+=1 
+        return a    
